6c16241a0689f321733ca7791baed37422230f8a
HarshithVR <118912568+HarshithVR@users.noreply.github.com>
2022-12-29 11:05:56 +0530

    Create Surface3D.py

---
+++ Surface3D.py
@@ -0,0 +1,11 @@
+import math
+
+
+def cube(side):
+	area = 6 * side * side
+
+def cuboid(length, breadth, height):
+	area = 2 * (length * breadth + breadth * height + length * height)
+
+def cone(radius, height):
+	area = 3.14 * radius(radius + 0.5**((height * height) + (radius * radius))
