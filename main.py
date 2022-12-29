5ed988bc3298ba38b5793641c053b6f74be65bb2
HarshithVR <118912568+HarshithVR@users.noreply.github.com>
2022-12-29 12:17:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from tkinter import *
 
 from tkinter import ttk
 
-from functions import *
-
 root = tk.Tk()
 
 root.geometry("550x350")
@@ -14,6 +12,15 @@ root.title("Area Calculator")
 
 label = ttk.Label(root, text='Dimension', font=10, foreground="#273746")
 label.grid(column=0, row=0, padx=20, pady=10,  sticky='w')
+
+'''shape_dimensions = ["2D", "3D"]
+
+variable = StringVar(root)
+variable.set(shape_dimensions[0])
+
+dropdown = ttk.OptionMenu(root, variable, *shape_dimensions,font=10, foreground="#273746")
+dropdown.grid(column=0, row=0, padx=500, pady=10,  sticky='w')'''
+
 
 label = ttk.Label(root, text='2D', font=10, foreground="#273746")
 label.grid(column=0, row=0, padx=500, pady=10,  sticky='w')
