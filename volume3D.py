71225f97d92dd9873f94331684722ce0c42f6153
HarshithVR <118912568+HarshithVR@users.noreply.github.com>
2022-12-29 11:44:58 +0530

    Update volume3D.py

--- volume3D.py
+++ volume3D.py
@@ -1,10 +1,25 @@
 import math
 
 def cube(side):
-	volume = side * side * side
+	volume = side **3
 
 def cuboid(length, breadth, height):
 	volume = length * breadth * height
 
 def cone(radius, height):
-	volume = 0.333 * 3.14 * (radius * radius) * height
+	volume = 0.333 * 3.14 * (radius ** 2) * height
+
+def cylinder(radius, height):
+	volume = 3.14 * (radius ** 2) * height
+
+def sphere(radius):
+	volume = 1.333 * 3.14 * (radius ** 3)
+
+def hemisphere(radius):
+	volume = 0.6667 * 3.14 * (radius ** 3)
+
+def tetrahedron(side):
+	volume = (side ** 3) / 6 * math.sqrt(2)
+
+def pyramid(length, width, height):
+	volume = (length * width * height) / 3
