c9bff387406a237dd9b20cd9095a1c88c9781f43
HarshithVR <118912568+HarshithVR@users.noreply.github.com>
2022-12-29 11:39:41 +0530

    Update surface3D.py

--- surface3D.py
+++ surface3D.py
@@ -2,22 +2,32 @@ import math
 
 
 def cube(side):
-	area = 6 * side * side
+	area = 6 * side ** 2
+	
 
 def cuboid(length, breadth, height):
 	area = 2 * (length * breadth + breadth * height + length * height)
+	
 
 def cone(radius, height):
-	area = 3.14 * radius(radius + math.sqrt((height * height) + (radius * radius))
+	area = 3.14 * radius(radius + math.sqrt((height ** 2) + (radius ** 2))
+			     
 
 def cylinder(radius, height):
-	area = 2 * 3.14 * radius * height + 2 * 3.14 * (radius * radius)
+	area = 2 * 3.14 * radius * height + 2 * 3.14 * (radius ** 2)
+			     
 
 def sphere(radius):
-	area = 4 * 3.14 * radius * radius
+	area = 4 * 3.14 * (radius ** 2)
+			     
 
 def hemisphere(radius):
-	area = 2 * 3.14 * radius * radius
+	area = 2 * 3.14 * (radius **2)
+			     
 
 def tetrahedron(side):
-	area = math.sqrt(3) * side * side
+	area = math.sqrt(3) * (side ** 2)
+			     
+
+def pyramid(length, width, heigth):
+	area = length * width + length * math.sqrt((width/2)**2 + height**2) + width * math.sqrt((length/2)**2 + height ** 2)
