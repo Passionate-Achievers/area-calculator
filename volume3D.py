f721be70508ab91ae0f9429ad145dad6030cae79
HarshithVR <118912568+HarshithVR@users.noreply.github.com>
2022-12-29 11:08:49 +0530

    Create volume3D.py

---
+++ volume3D.py
@@ -0,0 +1,10 @@
+import math
+
+def cube(side):
+	volume = side * side * side
+
+def cuboid(length, breadth, height):
+	volume = length * breadth * height
+
+def cone(radius, height):
+	volume = 0.333 * 3.14 * (radius * radius) * height
